3a38a56a02fbf441c310f9b64b74066b80e4a6bd
Shreya Ghimire <shreyaghimire9966@gmail.com>
2025-02-05 22:11:16 -0500

    Added ProductApp solution and project files

---
+++ ProductApp/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace ProductApp
+{
+    public class Product
+    {
+        public int ProdID { get; private set; }
+        public string ProdName { get; private set; }
+        public decimal ItemPrice { get; private set; }
+        public int StockAmount { get; private set; }
+
+        public Product(int prodID, string prodName, decimal itemPrice, int stockAmount)
+        {
+            if (prodID < 5 || prodID > 50000) throw new ArgumentOutOfRangeException(nameof(prodID));
+            if (itemPrice < 5 || itemPrice > 5000) throw new ArgumentOutOfRangeException(nameof(itemPrice));
+            if (stockAmount < 5 || stockAmount > 500000) throw new ArgumentOutOfRangeException(nameof(stockAmount));
+
+            ProdID = prodID;
+            ProdName = prodName;
+            ItemPrice = itemPrice;
+            StockAmount = stockAmount;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            if (amount <= 0) throw new ArgumentException("Amount must be greater than 0.");
+            StockAmount += amount;
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            if (amount <= 0) throw new ArgumentException("Amount must be greater than 0.");
+            if (StockAmount - amount < 0) throw new InvalidOperationException("Stock cannot be negative.");
+            StockAmount -= amount;
+        }
+    }
+}
